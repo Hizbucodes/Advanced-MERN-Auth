9e3739cf991c16423a94f41a0c54a2a004349d5d
Hizbucodes <106305260+Hizbucodes@users.noreply.github.com>
2025-01-06 20:20:43 +0530

    add MongoDB connection

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,14 @@
 import express from "express";
-
+import dotenv from "dotenv";
+import { connectDB } from "./db/connectDB.js";
 const app = express();
+dotenv.config();
 
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
 
 app.listen(3000, () => {
+  connectDB();
   console.log("Server is running on port: 3000");
 });
